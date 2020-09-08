107fdf4bb13bdb92043a1b4cfe00438bad416570
Shubham Rastogi <70432137+shubhamrastogi1810@users.noreply.github.com>
2020-09-08 23:51:49 +0530

    Add files via upload

---
+++ primenumbers1.1.py
@@ -0,0 +1,13 @@
+for i in range(1,1000,2):
+    f=0
+    for j in range(2,i):
+         if f == 1:
+            break
+         else:
+            if i%j==0:
+                f=1
+    if f == 0:
+        print(i)
+
+
+
