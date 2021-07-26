9944ec665dd57f7363428fb8cd69455acfd0a0b6
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-07-26 15:55:48 +0530

    Project Euler's problem solutions.

--- Project_Euler/problem1.py
+++ Project_Euler/problem1.py
@@ -1,1 +1,6 @@
-print("Hello World")
+""" this program will print the sum of multiples of 3 or 5 below 1000."""
+ADD = 0
+for i in range(1,1000):
+    if i%3==0 or i%5==0:
+        ADD+=i
+print(ADD)
