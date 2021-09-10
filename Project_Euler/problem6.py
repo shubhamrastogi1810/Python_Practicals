5838ea1fec3d463696cca60a0ac5ddfce510a7e7
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 14:06:50 +0530

    Sum Square difference

--- Project_Euler/problem6.py
+++ Project_Euler/problem6.py
@@ -1,16 +1,22 @@
 """
 The code computes the difference of sum of square and square of the sum of first 100 natural numbers
 """
+def main():
+    """
+    This function prints the difference of sum square and square of the sums of 100 natural numbers
+    """
+    # sum of the squares of first 100 natural numbers.
+    sumsq = 0
+    for i in range(1,101):
+        sumsq += i**2
 
-# sum of the squares of first 100 natural numbers.
-SUMSQ = 0
-for i in range(1,101):
-    SUMSQ+=i**2
 
+    # square of the sum of first 100 natural numbers.
+    sums = 0
+    for i in range(1,101):
+        sums += i
+    sums = sums**2
+    print(sums - sumsq)
 
-# square of the sum of first 100 natural numbers.
-SUMS = 0
-for i in range(1,101):
-    SUMS += i
-SUMS = SUMS**2
-print(SUMS - SUMSQ)
+if __name__ == "__main__":
+    main()
