4218832d7bd6f0508124a60b984edcbc7cb5c9ac
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 10:23:06 +0530

    Multiples of 3 or 5

--- Project_Euler/problem1.py
+++ Project_Euler/problem1.py
@@ -1,6 +1,13 @@
 """ this program will print the sum of multiples of 3 or 5 below 1000."""
-ADD = 0
-for i in range(1,1000):
-    if i%3==0 or i%5==0:
-        ADD+=i
-print(ADD)
+def main():
+    """
+    The main function to print sum of multiples of 3 or 5 below 1000.
+    """
+    add = 0
+    for i in range(1,1000):
+        if i%3==0 or i%5==0:
+            add+=i
+    print(add)
+
+if __name__ == "__main__":
+    main()
