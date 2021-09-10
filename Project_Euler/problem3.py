931a2a511ab7954c99f72692708c195e9618934d
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 10:33:01 +0530

    Largest Prime Factor

--- Project_Euler/problem3.py
+++ Project_Euler/problem3.py
@@ -1,24 +1,30 @@
 """
     This code will find the largest prime factor for the following number.
 """
-NUM = 600851475143
-def isprime(num):
+def main():
     """
-        This function will return bool value if input number is prime or not.
+    Function will find the largest prime factor for the number.
     """
-    flag = True
-    for i in range(2,int(num ** 0.5)+1):
-        if num % i == 0:
-            flag = False
-            break
-    return bool(flag)
+    num = 600851475143
+    def isprime(number):
+        """
+            This function will return bool value if input number is prime or not.
+        """
+        flag = True
+        for i in range(2,int(number ** 0.5)+1):
+            if number % i == 0:
+                flag = False
+                break
+        return bool(flag)
 
-FACTORS = []
-for temp in range(3,int(NUM**0.5)+1):
-    if NUM % temp == 0:
-        FACTORS.append(temp)
-FACTORS.reverse()
-for j in FACTORS:
-    if isprime(j):
-        print(j)
-        break
+    factors = []
+    for temp in range(3,int(num**0.5)+1):
+        if num % temp == 0:
+            factors.append(temp)
+    factors.reverse()
+    for j in factors:
+        if isprime(j):
+            print(j)
+            break
+if __name__ == "__main__":
+    main()
