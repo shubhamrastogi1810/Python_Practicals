f71381681f782c6cbbea34561490ac75094af66d
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 14:11:06 +0530

    10001st prime

--- Project_Euler/problem7.py
+++ Project_Euler/problem7.py
@@ -1,15 +1,22 @@
 """
-    this program calculates the 10001th prime number.
+    This program calculates the 10001th prime number.
 """
-lis = [2,3]
-NUM = 5
-while len(lis) != 10001:
-    FLAG = True
-    for j in range(2,int(NUM**0.5)+1):
-        if NUM % j == 0:
-            FLAG = False
-            break
-    if FLAG is True:
-        lis.append(NUM)
-    NUM+=2
-print(lis[-1])
+def main():
+    """
+    This function prints 10001st prime number.
+    """
+    lis = [2,3]
+    num = 5
+    while len(lis) != 10001:
+        flag = True
+        for j in range(2,int(num**0.5)+1):
+            if num % j == 0:
+                flag = False
+                break
+        if flag is True:
+            lis.append(num)
+        num+=2
+    print(lis[-1])
+
+if __name__ == "__main__":
+    main()
