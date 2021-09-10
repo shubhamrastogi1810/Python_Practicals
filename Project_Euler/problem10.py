a02373411ebbe0b1dcf7f2a03e6cc6eb24e367bf
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 14:21:35 +0530

    Summation of primes

--- Project_Euler/problem10.py
+++ Project_Euler/problem10.py
@@ -1,13 +1,17 @@
 """
     This code will print the summation of prime numbers below 2 million.
 """
-SUMS = 2
-for i in range(3,2000000,2):
-    FLAG = False
-    for j in range(2,int(i**0.5)+1):
-        if i%j == 0:
-            FLAG = True
-            break
-    if FLAG is False:
-        SUMS+=i
-print(SUMS)
+def main():
+    """This code prints sums of prime numbers below 2 million."""
+    sums = 2
+    for i in range(3,2000000,2):
+        flag = False
+        for j in range(2,int(i**0.5)+1):
+            if i%j == 0:
+                flag = True
+                break
+        if flag is False:
+            sums+=i
+    print(sums)
+if __name__ == "__main__":
+    main()
