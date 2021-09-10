61811456df5ce6b1348b752d48072c904bdec409
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 14:20:45 +0530

    Factorial Digit Sum

--- Project_Euler/problem20.py
+++ Project_Euler/problem20.py
@@ -1,15 +1,21 @@
 """
     This code will compute sum of the digits of the 100!
 """
-# Finding the 100 Factorial.
-PROD = 1
-for i in range(2,101):
-    PROD*=i
+def main():
+    """
+    This function prints sum of the digits the 100!
+    """
+    # Finding the 100 Factorial.
+    prod = 1
+    for i in range(2,101):
+        prod*=i
 
-# Adding the Digits of 100!.
-SUM = 0
-while PROD > 0:
-    TEMP = PROD % 10
-    SUM+=TEMP
-    PROD //=10
-print(SUM)
+    # Adding the Digits of 100!.
+    sums = 0
+    while prod > 0:
+        temp = prod % 10
+        sums+=temp
+        prod //=10
+    print(sums)
+if __name__ == "__main__":
+    main()
