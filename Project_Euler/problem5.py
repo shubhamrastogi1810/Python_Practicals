098110fb2f3f6ed6074a676a822de3a26d6d73d1
Shubham Rastogi <shubhamrastogi@gmail.com>
2021-09-10 12:39:45 +0530

    Smallest Multiple

--- Project_Euler/problem5.py
+++ Project_Euler/problem5.py
@@ -1,16 +1,24 @@
 """
     This code computes the smallest evenly divisible number from 1 to 20.
 """
-NUM = 9699690
-# multiplied all prime numbers. [2,3,5,7,11,13,17,19]
-while True:
-    FLAG = True
-    for i in [4,8,9,12,15,16,18,20,9699690]:
-    # checking for the remaining numbers including the prime product.
-        if NUM % i != 0:
-            FLAG = False
+def main():
+    """
+    This program prints the smallest evenly divisible number of first 20 natural numbers.
+    """
+
+    num = 9699690
+    # multiplied all prime numbers. [2,3,5,7,11,13,17,19]
+    while True:
+        flag = True
+        for i in [4,8,9,12,15,16,18,20,9699690]:
+        # checking for the remaining numbers including the prime product.
+            if num % i != 0:
+                flag = False
+                break
+        if flag is True:
+            print(num)
             break
-    if FLAG is True:
-        print(NUM)
-        break
-    NUM+=2 # all the numbers in the list are even
+        num+=2 # all the numbers in the list are even
+
+if __name__ == "__main__":
+    main()
