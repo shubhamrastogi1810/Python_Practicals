ae3371f6faa550ee90b4358a5f6dabfa1c18dfcb
shubhamrastogi1810 <shubhamrastogi@gmail.com>
2023-09-09 17:40:29 +0530

    create a alphabet pattern with string

---
+++ HackerRank/rangoli.py
@@ -0,0 +1,11 @@
+def print_rangoli(size):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    data = [alpha[i] for i in range(size)]
+    items = list(range(size))
+    items = items[:-1]+items[::-1]
+    for i in items:	
+        temp = data[-(i+1):]
+        row = temp[::-1]+temp[1:]
+        print("-".join(row).center(size*4-3, "-"))
+        
+print_rangoli(int(input()))
